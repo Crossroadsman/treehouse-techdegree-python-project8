3ed43c3d74efd3c30540fa662ae63dc26c9f20c7
CrossroadsMan <crossroadsman@gmail.com>
2018-09-24 10:19:58 -0600

    Get groups working

--- mineral_catalog/catalog/static/catalog/js/sidebar_menu.js
+++ mineral_catalog/catalog/static/catalog/js/sidebar_menu.js
@@ -1,1 +1,14 @@
-window.alert("Hello, world");
+const sidebar_menu = document.querySelector('#sidebar-left')
+
+sidebar_menu.addEventListener('click', (event) => {
+    // If a title was clicked, identify it
+    // show/hide the adjacent UL
+    if (event.target.className == "sidebar-menuitem-title") {
+        let adjacent = event.target.nextElementSibling
+        if (adjacent.style.display === "") {
+            adjacent.style.display = 'none';
+        } else {
+            adjacent.style.display = '';
+        }
+    }
+});
